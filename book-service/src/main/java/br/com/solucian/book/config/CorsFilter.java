76110f2ae5370ece77dd519b3143084516aacb14
Luciano Barros <ferreira.obarros@gmail.com>
2025-12-20 20:14:40 -0300

    style: Removendo trash

--- book-service/src/main/java/br/com/solucian/book/config/CorsFilter.java
+++ book-service/src/main/java/br/com/solucian/book/config/CorsFilter.java
@@ -9,31 +9,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-//@Component
-//@Order(Ordered.HIGHEST_PRECEDENCE)
-//public class CorsFilter implements Filter {
-//
-//    @Override
-//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-//            throws IOException, ServletException {
-//
-//        HttpServletResponse response = (HttpServletResponse) res;
-//        HttpServletRequest request = (HttpServletRequest) req;
-//
-//        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-//        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-//        response.setHeader("Access-Control-Max-Age", "3600");
-//        response.setHeader("Access-Control-Allow-Headers", "*");
-//        response.setHeader("Access-Control-Allow-Credentials", "true");
-//
-//        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-//            response.setStatus(HttpServletResponse.SC_OK);
-//        } else {
-//            chain.doFilter(req, res);
-//        }
-//    }
-//}
-
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
